5d582f8052be84144bb200593555361277e83135
Owen Strong <owensanstrong@gmail.com>
2026-03-30 22:28:55 -0500

    Estimated age to be around 27.7 years

--- report7/spec_anal.py
+++ report7/spec_anal.py
@@ -79,6 +79,7 @@ target_labels = {
     "Background":"background"
 }
 
+
 # Expected E (keV): Centroid, FWHM, Net count rate, error : bf (energy), br (mode)
 id_peaks = {
     "137Cs":[
@@ -103,9 +104,34 @@ id_peaks = {
         "122:182.01,4.97,1102,41:85.60,100"
     ],
     "EuUnknown":[
-        "1408:2143.64,26.3,17031,384:20.87,72.08" # Positron
+        # "1408:2143.64,26.3,17031,384:20.87,72.08" # Positron
+        # "122:181.84,5.06,181140,1245:", # b+
+        # "244:369.22,6.83,32375,968:", # b+
+        # "344:521.26,7.72,55067,961:",
+        # "779:1183.91,15.09,17175,647:",
+        # "964:1466.18,18.52,16109,542:",
+        
+        # Using data from gammaspectacular
+        "1408:2143.64,26.3,17031,384:21.005,72.08" # b+
+        "122:181.84,5.06,181140,1245:28.58,72.08", # b+
+        "244:369.22,6.83,32375,968:7.583,72.08", # b+
+        "344:521.26,7.72,55067,961:26.5,27.92", # b-
+        "779:1183.91,15.09,17175,647:12.942,27.92",
+        "964:1466.18,18.52,16109,542:14.605,72.08", 
     ]
 }
+for iso in id_peaks:
+    print("______________________________")
+    print(f"{iso}:")
+    for line in id_peaks[iso]:
+        dat_sets = line.split(":")
+        N = int(dat_sets[1].split(",")[2])
+        Nstr = '{0:,}'.format(N)
+        Nerr = '{0:,}'.format(int(dat_sets[1].split(',')[3]))
+        E = dat_sets[0]
+        print(f"@ {" "*(5-len(E))}{E} kev: N = {" "*(10-len(Nstr))}{Nstr} +- {Nerr}")
+
+
 
 BASE_TIME_S = 600 # 1 microcurie samples were taken for 10 minutes
 DPY = 365.25
@@ -136,7 +162,25 @@ calpeaks = [ #Must be 1st in list
     # id_peaks["60Co"][0]
 ]
 
-known_isos = ["137Cs", "60Co", "54Mn", "133Ba", "57Co"]
+cy = [float(id_peaks[peak][0].split(":")[0]) for peak in calpeaks]
+cx = [float(id_peaks[peak][0].split(":")[1].split(",")[0]) for peak in calpeaks]
+m = (cy[1]-cy[0]) / (cx[1]-cx[0])
+b = cy[0]-(cx[0]*m)
+
+im = (cx[1] - cx[0]) / (cy[1]-cy[0])
+ib = cx[0] - (cy[0]*im)
+print(f"Calibration: {m:.5f} x + {b:.5f}")
+for eu_peak in id_peaks["EuUnknown"]:
+    bin = float(eu_peak.split(":")[1].split(",")[0])
+    E = m*bin + b
+    print(f"bin {int(bin)} -> {int(E)} keV")
+
+known_isos = [
+    "137Cs", "60Co", 
+    # "54Mn", 
+    "133Ba",
+    # "57Co",
+    ]
 
 MICROCURIE = 3.7e4
 BASE_ACTIVITY = MICROCURIE
@@ -184,41 +228,103 @@ def abs_eff(iso, E_int):
     return None, None, None
 
 print(f"S = A * T * (BF * BR)")
-E_list = []
-aeff_list = []
-aerr_list = []
+all_E = []
+all_aeff = []
+all_aerr = []
 for iso in known_isos:
+    E_list = []
+    aeff_list = []
+    aerr_list = []
     peaks = id_peaks[iso]
     for peak in peaks:
         E = int(peak.split(":")[0])
         E, eff, err = abs_eff(iso, E)
         print(f"Efficiency at {E} keV: {eff:.5f} +- {err:.5f}")
         E_list.append(E)
+        all_E.append(E)
         aeff_list.append(eff)
+        all_aeff.append(eff)
         aerr_list.append(err)
-E_list = np.array(E_list)
-aeff_list = np.array(aeff_list)*100
-aerr_list = np.array(aerr_list)*100
-plt.scatter(E_list, aeff_list, label="Absolute efficiency", marker="x")
+        all_aerr.append(err)
+    E_list = np.array(E_list)
+    aeff_list = np.array(aeff_list)*100
+    aerr_list = np.array(aerr_list)*100
+    plt.scatter(E_list, aeff_list, label=f"{iso}", marker="x")
 # l0, = plt.plot(0, 0, marker='o', color='b')
-(_, caps, _) = plt.errorbar(E_list, aeff_list, yerr=aerr_list, label="Error", capsize=10, elinewidth=1, fmt="none")
-for cap in caps:
-    cap.set_color('red')
-    cap.set_markeredgewidth(1)
+    (_, caps, _) = plt.errorbar(E_list, aeff_list, yerr=aerr_list, capsize=10, elinewidth=1, fmt="none")
+    for cap in caps:
+        cap.set_color('black')
+        cap.set_markeredgewidth(1)
+
+ln_E = np.log(np.array(all_E))
+ln_aeff = np.log(np.array(all_aeff))
+all_aerr = np.log(np.array(all_aerr))
+N_TERMS = 4
+poly = np.polynomial.polynomial.Polynomial.fit(ln_E, ln_aeff, N_TERMS-1, domain=[-1,1], w=1/all_aerr)
+print(poly)
+ln_E_range = np.linspace(min(ln_E), max(ln_E), 1000)
+ln_aeff_fit = poly(ln_E_range)
+print(f"{poly(0)}")
+print(f"{poly(10)}")
+# print(f"{np.polyval(poly.convert().coef, 0)}")
+
+Eu_Es = []
+Eu_effs = []
+Eu_ages = []
+decays_Eu = []
+decay_vars_Eu = []
+for peak in id_peaks["EuUnknown"]:
+    dat_Eu = peak.split(":")
+
+    E_Eu = int(dat_Eu[0])
+    aeff_Eu = np.exp(poly(np.log(E_Eu)))
+    Eu_Es.append(E_Eu)
+    Eu_effs.append(aeff_Eu)
+    print("_"*30)
+    print(f"Efficiency @ {E_Eu} keV: {aeff_Eu:.5f}")
+    # eff = N / S
+    N_Eu = int(dat_Eu[1].split(",")[2])
+    Nerr_Eu = int(dat_Eu[1].split(",")[3])
+    
+    S_Eu = int(N_Eu / aeff_Eu)
+    Serr_Eu = int(Nerr_Eu / aeff_Eu)
+    print(f"152Eu @ {E_Eu} keV: N = {N_Eu} +- {Nerr_Eu} -> S = {S_Eu} +- {Serr_Eu}")
+    # S = A * T * TBF
+    BF_Eu = float(dat_Eu[2].split(",")[0]) / 100
+    BR_Eu = float(dat_Eu[2].split(",")[1]) / 100
+    TB_Eu = BF_Eu
+    T_Eu = 300
+    A_Eu = np.array([S_Eu-Serr_Eu, S_Eu, S_Eu+Serr_Eu]) / (TB_Eu * T_Eu)
+    OG_A_Eu = 10*MICROCURIE
+    decay_Eu = A_Eu / OG_A_Eu
+    decays_Eu.append(decay_Eu[1])
+    decay_vars_Eu.append((decay_Eu[1]-decay_Eu[0])**2)
+    lamb_Eu = np.log(2) / half_lives_y["EuUnknown"]
+    elap_Eu = np.log(decay_Eu) / (-lamb_Eu)
+    Eu_ages.append(elap_Eu)
+    print(f"From A of {OG_A_Eu:,.3f} to {A_Eu} -> {elap_Eu} years elapsed")
+
+mean_decay = sum(decays_Eu) / len(decays_Eu)
+var_decay = sum(decay_vars_Eu) / (len(decays_Eu)**2)
+sig_decay = var_decay**0.5
+print(f"sig: {sig_decay}")
+decay_range = np.array([mean_decay+sig_decay, mean_decay, mean_decay-sig_decay])
+lamb_Eu = np.log(2) / half_lives_y["EuUnknown"]
+elap_range = np.log(decay_range) / (-lamb_Eu)
+print(elap_range[1:3]-elap_range[0:2])
+print(f"{elap_range} years passed")
+
+plt.plot(np.exp(ln_E_range), np.exp(ln_aeff_fit)*100, label=f"{N_TERMS} term ln fit")
+# plt.plot(np.exp(ln_E), np.exp(poly(ln_E))*100, label="Four term fit 2")
+plt.scatter(np.array(Eu_Es), np.array(Eu_effs)*100, label="Est. for 152Eu")
+
 plt.ylabel("Absolute Efficiency (%)")
 plt.xlabel("Energy (keV)")
+plt.loglog()
 plt.legend()
 plt.show()
 quit()
 
-cy = [float(id_peaks[peak][0].split(":")[0]) for peak in calpeaks]
-cx = [float(id_peaks[peak][0].split(":")[1].split(",")[0]) for peak in calpeaks]
-m = (cy[1]-cy[0]) / (cx[1]-cx[0])
-b = cy[0]-(cx[0]*m)
-
-im = (cx[1] - cx[0]) / (cy[1]-cy[0])
-ib = cx[0] - (cy[0]*im)
-print(f"Calibration: {m:.5f} x + {b:.5f}")
 
 for target in id_peaks:
     for peak in id_peaks[target]:
